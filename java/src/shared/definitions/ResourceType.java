4a9cf172f7737466e06cb582bd26657cfa761891
lasaldan <lasaldan@gmail.com>
2014-05-09 09:12:47 -0600

    Added some Javadocs to Card Classes

--- java/src/shared/definitions/ResourceType.java
+++ java/src/shared/definitions/ResourceType.java
@@ -1,7 +1,69 @@
 package shared.definitions;
 
-public enum ResourceType {
+import client.models.IResourceCard;
 
-	WOOD, BRICK, SHEEP, WHEAT, ORE
+import java.awt.image.BufferedImage;
+
+public enum ResourceType implements IResourceCard {
+
+	WOOD {
+        @Override
+        public BufferedImage getImage() {
+            return null;
+        }
+
+        @Override
+        public String getName() {
+            return null;
+        }
+    },
+
+    BRICK {
+        @Override
+        public BufferedImage getImage() {
+            return null;
+        }
+
+        @Override
+        public String getName() {
+            return null;
+        }
+    },
+
+    SHEEP {
+        @Override
+        public BufferedImage getImage() {
+            return null;
+        }
+
+        @Override
+        public String getName() {
+            return null;
+        }
+    },
+
+    WHEAT {
+        @Override
+        public BufferedImage getImage() {
+            return null;
+        }
+
+        @Override
+        public String getName() {
+            return null;
+        }
+    },
+
+    ORE {
+        @Override
+        public BufferedImage getImage() {
+            return null;
+        }
+
+        @Override
+        public String getName() {
+            return null;
+        }
+    }
 }
 
