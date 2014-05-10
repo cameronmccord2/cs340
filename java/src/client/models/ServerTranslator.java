6f9a68d28aaddf018766c55cdb952620f7f89e5a
Cameron McCord <cameronmccord2@gmail.com>
2014-05-09 21:58:26 -0600

    Added server json translator

---
+++ java/src/client/models/ServerTranslator.java
@@ -0,0 +1,17 @@
+package client.models;
+
+/**
+ * The Class ServerTranslator.
+ */
+public class ServerTranslator {
+
+	/**
+	 * Translate json into game model using Gson
+	 *
+	 * @param json the json from the server to translate
+	 * @return the game object
+	 */
+	public IGame translateJsonIntoGameModel(String json){
+		return null;
+	}
+}
