59eabae7e810339d6744483db04a7c5edfce755c
callyalater <cwc223@byu.edu>
2014-05-14 04:57:45 -0600

    Resolving merge conflicts

--- java/src/client/models/ILocation.java
+++ java/src/client/models/ILocation.java
@@ -1,12 +1,9 @@
 package client.models;
 
-<<<<<<< HEAD
 import shared.locations.*;
 import shared.definitions.*;
 
 public interface ILocation
-=======
->>>>>>> de95b82f55911934df8e276a272baba3d17818fc
 {
 	public HexLocation getHexLocation();
 	public void setHexLocation(HexLocation location);
