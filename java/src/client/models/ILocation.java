99539f49fcce1eae1685e9577d86813aa76c5465
callyalater <cwc223@byu.edu>
2014-05-14 04:51:20 -0600

    I'm starting to flush out a working body for the CatanMap class and all of the subimplementations for the smaller classes.

--- java/src/client/models/ILocation.java
+++ java/src/client/models/ILocation.java
@@ -3,7 +3,14 @@ package client.models;
 import shared.locations.*;
 import shared.definitions.*;
 
-public interface ILocation 
+public interface ILocation
 {
-	
+	public HexLocation getHexLocation();
+	public void setHexLocation(HexLocation location);
+
+	public EdgeDirection getEdgeDirection();
+	public void setEdgeDirection(EdgeDirection edge);
+
+	public VertexDirection getVertexDirection();
+	public void setVertexDirection(VertexDirection vertex);
 }
---
+++ java/src/client/models/IPiece.java
@@ -0,0 +1,7 @@
+package client.models;
+
+public interface IPiece
+{
+	public ILocation getLocation();
+	public void setLocation(ILocation location);
+}
