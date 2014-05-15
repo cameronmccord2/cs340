2830a4b3058762fa66ee48d397e802faf3f6f2cc
Scott Daly <scottdaly@Khan.local>
2014-05-14 22:06:52 -0600

    created DummyProxy

--- java/src/client/models/DummyProxy.java
+++ java/src/client/models/DummyProxy.java
@@ -5,7 +5,6 @@ package client.models;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import client.data.PlayerInfo;
 import client.exceptions.InvalidGameModelException;
@@ -45,6 +44,7 @@ public class DummyProxy implements IProxy{
 	private Gson gson = new Gson();
 	private Translator translator;
 	private List<IGame> games;
+	private String gameId = "0";
 	
 	public DummyProxy() {
 		this.translator = new Translator();
@@ -65,25 +65,21 @@ public class DummyProxy implements IProxy{
 	
 	@Override
 	public ServerResponse postUserLogin(User user){
-		
+		ServerResponse sr = new ServerResponse("Success",200);
 		//saveGameModel(sr.getJson());
-		Map<String, List<String>> map = connection.getHeaderFields();
-		List<String> setCookie = map.get("Set-cookie");
-		cookie = setCookie.get(0);
-		cookie = cookie.substring(0, cookie.length() - 8);
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse postUserRegister(User user){
-		ServerResponse sr = doPost("/user/register", gson.toJson(user));
+		ServerResponse sr = new ServerResponse("Success",200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public GameServer[] getGamesList(){
-		ServerResponse sr = doGet("/games/list");
+		ServerResponse sr = new ServerResponse("[{'title': 'Default Game','id': 0,'players': [{'color': 'orange','name':'Sam','id': 0},{'color':'blue','name': 'Brooke','id': 1},{'color':'red','name':'Pete','id':10},{'color':'green','name':'Mark','id': 11}]}]",200);
 		List<GameServer> games = gson.fromJson(sr.getJson(), new TypeToken<List<GameServer>>(){}.getType());
 		GameServer[] list = new GameServer[games.size()];
 		int index = 0;
@@ -96,19 +92,15 @@ public class DummyProxy implements IProxy{
 	
 	@Override
 	public ServerResponse postGamesCreate(CreateGame game){
-		ServerResponse sr = doPost("/games/create", gson.toJson(game));
+		ServerResponse sr = new ServerResponse("{'title':'MINE','id': 3,'players': [{},{},{},{}]}",200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse postGamesJoin(ServerJoinGame join){
-		ServerResponse sr = doJoinPost("/games/join", gson.toJson(join));
+		ServerResponse sr = new ServerResponse("Success",200);
 		//saveGameModel(sr.getJson());
-		Map<String, List<String>> map = connection.getHeaderFields();
-		List<String> setCookie = map.get("Set-cookie");
-		gameId = setCookie.get(0);
-		gameId = gameId.substring(0, gameId.length() - 8);
 		return sr;
 	}
 	
@@ -123,7 +115,7 @@ public class DummyProxy implements IProxy{
 		String requestUrl = "/game/model";
 		if(version != 0)
 			requestUrl += "?version=" + version;
-		ServerResponse sr = this.doGet(requestUrl);
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		this.saveGameModel(sr.getJson());
 	}
 	
@@ -145,119 +137,119 @@ public class DummyProxy implements IProxy{
 
 	@Override
 	public ServerResponse postGameReset(){
-		ServerResponse sr = doMasterPost("/game/reset","");
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse movesSendChat(ServerChat chat){
-		ServerResponse sr = doMasterPost("/moves/sendChat", gson.toJson(chat));
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse movesRollNumber(ServerRoll roll){
-		ServerResponse sr = doMasterPost("/moves/rollNumber", gson.toJson(roll));
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse moveRobPlayer(ServerRobPlayer rob){
-		ServerResponse sr = doMasterPost("/moves/robPlayer", gson.toJson(rob));
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse movesFinishTurn(FinishedTurn turn){
-		ServerResponse sr = doMasterPost("/moves/finishTurn", gson.toJson(turn));
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse movesBuyDevCard(BuyDevCard card){
-		ServerResponse sr = doMasterPost("/moves/buyDevCard", gson.toJson(card));
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse movesYear_of_Plenty(ServerYearofPlenty yop){
-		ServerResponse sr = doMasterPost("/moves/Year_of_Plenty", gson.toJson(yop));
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse movesRoad_Building(RoadBuilding rb){
-		ServerResponse sr = doMasterPost("/moves/Road_Building", gson.toJson(rb));
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse movesSoldier(ServerSoldier ss){
-		ServerResponse sr = doMasterPost("/moves/Soldier", gson.toJson(ss));
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse movesMonopoly(ServerMonopoly sm){
-		ServerResponse sr = doMasterPost("/moves/Monopoly", gson.toJson(sm));
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse movesMonument(ServerMonument sm){
-		ServerResponse sr = doMasterPost("/moves/Monument", gson.toJson(sm));
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse movesBuildRoad(ServerBuildRoad br){
-		ServerResponse sr = doMasterPost("/moves/buildRoad", gson.toJson(br));
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse movesBuildSettlement(ServerBuildSettlement bs){
-		ServerResponse sr = doMasterPost("/moves/buildSettlement", gson.toJson(bs));
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse movesBuildCity(ServerBuildCity bc){
-		ServerResponse sr = doMasterPost("/moves/buildCity", gson.toJson(bc));
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse movesOfferTrade(OfferTrade ot){
-		ServerResponse sr = doMasterPost("/moves/offerTrade", gson.toJson(ot));
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse movesAcceptTrade(AcceptTrade at){
-		ServerResponse sr = doMasterPost("/moves/acceptTrade", gson.toJson(at));
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
 	
 	@Override
 	public ServerResponse movesMaritimeTrade(MaritimeTradeOff mTrade){
-		ServerResponse sr = doMasterPost("/moves/maritimeTrade", gson.toJson(mTrade));
+		ServerResponse sr = new ServerResponse(updateModel,200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
@@ -269,8 +261,10 @@ public class DummyProxy implements IProxy{
 	
 	@Override
 	public ServerResponse utilChangeLogLevel(ServerLogLevel loglevel){
-		ServerResponse sr = doMasterPost("/util/changeLogLevel", gson.toJson(loglevel));
+		ServerResponse sr = new ServerResponse("Succes",200);
 		//saveGameModel(sr.getJson());
 		return sr;
 	}
+	
+	private String updateModel = "{ 'deck': { 'yearOfPlenty': 2, 'monopoly': 2, 'soldier': 14, 'roadBuilding': 2, 'monument': 5 }, 'map': { 'hexes': [ { 'location': { 'x': 0, 'y': -2 } }, { 'resource': 'brick', 'location': { 'x': 1, 'y': -2 }, 'number': 4 }, { 'resource': 'wood', 'location': { 'x': 2, 'y': -2 }, 'number': 11 }, { 'resource': 'brick', 'location': { 'x': -1, 'y': -1 }, 'number': 8 }, { 'resource': 'wood', 'location': { 'x': 0, 'y': -1 }, 'number': 3 }, { 'resource': 'ore', 'location': { 'x': 1, 'y': -1 }, 'number': 9 }, { 'resource': 'sheep', 'location': { 'x': 2, 'y': -1 }, 'number': 12 }, { 'resource': 'ore', 'location': { 'x': -2, 'y': 0 }, 'number': 5 }, { 'resource': 'sheep', 'location': { 'x': -1, 'y': 0 }, 'number': 10 }, { 'resource': 'wheat', 'location': { 'x': 0, 'y': 0 }, 'number': 11 }, { 'resource': 'brick', 'location': { 'x': 1, 'y': 0 }, 'number': 5 }, { 'resource': 'wheat', 'location': { 'x': 2, 'y': 0 }, 'number': 6 }, { 'resource': 'wheat', 'location': { 'x': -2, 'y': 1 }, 'number': 2 }, { 'resource': 'sheep', 'location': { 'x': -1, 'y': 1 }, 'number': 9 }, { 'resource': 'wood', 'location': { 'x': 0, 'y': 1 }, 'number': 4 }, { 'resource': 'sheep', 'location': { 'x': 1, 'y': 1 }, 'number': 10 }, { 'resource': 'wood', 'location': { 'x': -2, 'y': 2 }, 'number': 6 }, { 'resource': 'ore', 'location': { 'x': -1, 'y': 2 }, 'number': 3 }, { 'resource': 'wheat', 'location': { 'x': 0, 'y': 2 }, 'number': 8 } ], 'roads': [ { 'owner': 2, 'location': { 'direction': 'S', 'x': 1, 'y': -1 } }, { 'owner': 3, 'location': { 'direction': 'SW', 'x': 2, 'y': -2 } }, { 'owner': 0, 'location': { 'direction': 'S', 'x': 0, 'y': 1 } }, { 'owner': 1, 'location': { 'direction': 'SW', 'x': -2, 'y': 1 } }, { 'owner': 2, 'location': { 'direction': 'S', 'x': 0, 'y': 0 } }, { 'owner': 0, 'location': { 'direction': 'SW', 'x': 2, 'y': 0 } }, { 'owner': 1, 'location': { 'direction': 'S', 'x': -1, 'y': -1 } }, { 'owner': 3, 'location': { 'direction': 'SW', 'x': -1, 'y': 1 } } ], 'cities': [], 'settlements': [ { 'owner': 3, 'location': { 'direction': 'SE', 'x': 1, 'y': -2 } }, { 'owner': 2, 'location': { 'direction': 'SW', 'x': 0, 'y': 0 } }, { 'owner': 2, 'location': { 'direction': 'SW', 'x': 1, 'y': -1 } }, { 'owner': 1, 'location': { 'direction': 'SW', 'x': -1, 'y': -1 } }, { 'owner': 0, 'location': { 'direction': 'SE', 'x': 0, 'y': 1 } }, { 'owner': 1, 'location': { 'direction': 'SW', 'x': -2, 'y': 1 } }, { 'owner': 0, 'location': { 'direction': 'SW', 'x': 2, 'y': 0 } }, { 'owner': 3, 'location': { 'direction': 'SW', 'x': -1, 'y': 1 } } ], 'radius': 3, 'ports': [ { 'ratio': 2, 'resource': 'wheat', 'direction': 'S', 'location': { 'x': -1, 'y': -2 } }, { 'ratio': 3, 'direction': 'SW', 'location': { 'x': 3, 'y': -3 } }, { 'ratio': 2, 'resource': 'ore', 'direction': 'S', 'location': { 'x': 1, 'y': -3 } }, { 'ratio': 3, 'direction': 'SE', 'location': { 'x': -3, 'y': 0 } }, { 'ratio': 2, 'resource': 'wood', 'direction': 'NE', 'location': { 'x': -3, 'y': 2 } }, { 'ratio': 2, 'resource': 'brick', 'direction': 'NE', 'location': { 'x': -2, 'y': 3 } }, { 'ratio': 3, 'direction': 'NW', 'location': { 'x': 2, 'y': 1 } }, { 'ratio': 2, 'resource': 'sheep', 'direction': 'NW', 'location': { 'x': 3, 'y': -1 } }, { 'ratio': 3, 'direction': 'N', 'location': { 'x': 0, 'y': 3 } } ], 'robber': { 'x': 0, 'y': -2 } }, 'players': [ { 'resources': { 'brick': 0, 'wood': 1, 'sheep': 1, 'wheat': 1, 'ore': 0 }, 'oldDevCards': { 'yearOfPlenty': 0, 'monopoly': 0, 'soldier': 0, 'roadBuilding': 0, 'monument': 0 }, 'newDevCards': { 'yearOfPlenty': 0, 'monopoly': 0, 'soldier': 0, 'roadBuilding': 0, 'monument': 0 }, 'roads': 13, 'cities': 4, 'settlements': 3, 'soldiers': 0, 'victoryPoints': 2, 'monuments': 0, 'playedDevCard': false, 'discarded': false, 'playerID': 0, 'playerIndex': 0, 'name': 'Sam', 'color': 'orange' }, { 'resources': { 'brick': 1, 'wood': 0, 'sheep': 1, 'wheat': 0, 'ore': 1 }, 'oldDevCards': { 'yearOfPlenty': 0, 'monopoly': 0, 'soldier': 0, 'roadBuilding': 0, 'monument': 0 }, 'newDevCards': { 'yearOfPlenty': 0, 'monopoly': 0, 'soldier': 0, 'roadBuilding': 0, 'monument': 0 }, 'roads': 13, 'cities': 4, 'settlements': 3, 'soldiers': 0, 'victoryPoints': 2, 'monuments': 0, 'playedDevCard': false, 'discarded': false, 'playerID': 1, 'playerIndex': 1, 'name': 'Brooke', 'color': 'blue' }, { 'resources': { 'brick': 0, 'wood': 1, 'sheep': 1, 'wheat': 1, 'ore': 0 }, 'oldDevCards': { 'yearOfPlenty': 0, 'monopoly': 0, 'soldier': 0, 'roadBuilding': 0, 'monument': 0 }, 'newDevCards': { 'yearOfPlenty': 0, 'monopoly': 0, 'soldier': 0, 'roadBuilding': 0, 'monument': 0 }, 'roads': 13, 'cities': 4, 'settlements': 3, 'soldiers': 0, 'victoryPoints': 2, 'monuments': 0, 'playedDevCard': false, 'discarded': false, 'playerID': 10, 'playerIndex': 2, 'name': 'Pete', 'color': 'red' }, { 'resources': { 'brick': 0, 'wood': 1, 'sheep': 1, 'wheat': 0, 'ore': 1 }, 'oldDevCards': { 'yearOfPlenty': 0, 'monopoly': 0, 'soldier': 0, 'roadBuilding': 0, 'monument': 0 }, 'newDevCards': { 'yearOfPlenty': 0, 'monopoly': 0, 'soldier': 0, 'roadBuilding': 0, 'monument': 0 }, 'roads': 13, 'cities': 4, 'settlements': 3, 'soldiers': 0, 'victoryPoints': 2, 'monuments': 0, 'playedDevCard': false, 'discarded': false, 'playerID': 11, 'playerIndex': 3, 'name': 'Mark', 'color': 'green' } ], 'log': { 'lines': [ { 'source': 'Sam', 'message': 'Sam built a road' }, { 'source': 'Sam', 'message': 'Sam built a settlement' }, { 'source': 'Sam', 'message': 'Sam's turn just ended' }, { 'source': 'Brooke', 'message': 'Brooke built a road' }, { 'source': 'Brooke', 'message': 'Brooke built a settlement' }, { 'source': 'Brooke', 'message': 'Brooke's turn just ended' }, { 'source': 'Pete', 'message': 'Pete built a road' }, { 'source': 'Pete', 'message': 'Pete built a settlement' }, { 'source': 'Pete', 'message': 'Pete's turn just ended' }, { 'source': 'Mark', 'message': 'Mark built a road' }, { 'source': 'Mark', 'message': 'Mark built a settlement' }, { 'source': 'Mark', 'message': 'Mark's turn just ended' }, { 'source': 'Mark', 'message': 'Mark built a road' }, { 'source': 'Mark', 'message': 'Mark built a settlement' }, { 'source': 'Mark', 'message': 'Mark's turn just ended' }, { 'source': 'Pete', 'message': 'Pete built a road' }, { 'source': 'Pete', 'message': 'Pete built a settlement' }, { 'source': 'Pete', 'message': 'Pete's turn just ended' }, { 'source': 'Brooke', 'message': 'Brooke built a road' }, { 'source': 'Brooke', 'message': 'Brooke built a settlement' }, { 'source': 'Brooke', 'message': 'Brooke's turn just ended' }, { 'source': 'Sam', 'message': 'Sam built a road' }, { 'source': 'Sam', 'message': 'Sam built a settlement' }, { 'source': 'Sam', 'message': 'Sam's turn just ended' } ] }, 'chat': { 'lines': [] }, 'bank': { 'brick': 23, 'wood': 21, 'sheep': 20, 'wheat': 22, 'ore': 22 }, 'turnTracker': { 'status': 'Rolling', 'currentTurn': 0, 'longestRoad': -1, 'largestArmy': -1 }, 'winner': -1, 'version': 0 }";
 }
