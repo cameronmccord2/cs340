8e358968bf5e83450102566ff9179744d9e548e2
Scott Daly <scottdaly@Khan.local>
2014-05-22 20:00:53 -0600

    discard endpoint setup

---
+++ java/src/client/server/DiscardCard.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package client.server;
+
+import client.models.translator.TRResourceList;
+
+/**
+ * Holds information to discard certain cards to server
+ * @author scottdaly
+ *
+ */
+public class DiscardCard {
+	private String type;
+	private Integer playerIndex;
+	private TRResourceList discardedCards;
+	public DiscardCard(String type, Integer playerIndex,
+			TRResourceList discardedCards) {
+		this.type = type;
+		this.playerIndex = playerIndex;
+		this.discardedCards = discardedCards;
+	}
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+	/**
+	 * @return the playerIndex
+	 */
+	public Integer getPlayerIndex() {
+		return playerIndex;
+	}
+	/**
+	 * @param playerIndex the playerIndex to set
+	 */
+	public void setPlayerIndex(Integer playerIndex) {
+		this.playerIndex = playerIndex;
+	}
+	/**
+	 * @return the discardedCards
+	 */
+	public TRResourceList getDiscardedCards() {
+		return discardedCards;
+	}
+	/**
+	 * @param discardedCards the discardedCards to set
+	 */
+	public void setDiscardedCards(TRResourceList discardedCards) {
+		this.discardedCards = discardedCards;
+	}
+	
+	
+}
