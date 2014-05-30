380df1dc002774493b1c398a59e0a3dadf3efca9
Cameron McCord <cameronmccord2@gmail.com>
2014-05-29 23:00:20 -0600

    updated more javadoc

--- java/src/server/facades/IGameFacade.java
+++ java/src/server/facades/IGameFacade.java
@@ -10,36 +10,36 @@ public interface IGameFacade extends IServerModelFacade {
 	/**
 	 * Gets the list of commands that have been execuded for the specified game.
 	 *
-	 * @param json the json
-	 * @param ua the ua
-	 * @return the commands
+	 * @param json the json indicating which game to get the commands for
+	 * @param ua the user attributes from the requesting user
+	 * @return the commands execuded in this game
 	 */
 	public String getCommands(String json, UserAttributes ua);
 	
 	/**
 	 * Run commands that are in the format that the getCommands endponit returns. Allows replaying of events.
 	 *
-	 * @param json the json
-	 * @param ua the ua
-	 * @return the string
+	 * @param json a json array of commands to execute
+	 * @param ua the user attributes from the requesting user
+	 * @return the string "Successful" or an error
 	 */
 	public String runCommands(String json, UserAttributes ua);
 	
 	/**
 	 * Resets the game model to the create game state.
 	 *
-	 * @param json the json
-	 * @param ua the ua
-	 * @return the string
+	 * @param json the json indicating which game id
+	 * @param ua the user attributes from the requesting user
+	 * @return the string "Successful"
 	 */
 	public String reset(String json, UserAttributes ua);
 	
 	/**
 	 * Gets the game model that needs to be sent to the client for gameplay.
 	 *
-	 * @param json the json
-	 * @param ua the ua
-	 * @return the game model
+	 * @param json the json representing the game model version
+	 * @param ua the user attributes from the requesting user
+	 * @return the game model as a json string
 	 */
 	public String getGameModel(String json, UserAttributes ua);
 	
--- java/src/server/facades/IGamesFacade.java
+++ java/src/server/facades/IGamesFacade.java
@@ -2,16 +2,54 @@ package server.facades;
 
 import server.models.UserAttributes;
 
+/**
+ * The Interface IGamesFacade.
+ */
 public interface IGamesFacade extends IServerModelFacade {
 
+	/**
+	 * Lists the games that are currently in the server
+	 *
+	 * @param json not really anything for this endpoint
+	 * @param ua the user attributes from the requesting user
+	 * @return the json string of the games in the server
+	 */
 	public String listGames(String json, UserAttributes ua);
 	
+	/**
+	 * Creates the game based on the flags in the json
+	 *
+	 * @param json the json that indicates how to set up the game
+	 * @param ua the user attributes from the requesting user
+	 * @return the json string of the game model
+	 */
 	public String createGame(String json, UserAttributes ua);
 	
+	/**
+	 * Join game that already exists
+	 *
+	 * @param json the json indicating which game to join
+	 * @param ua the user attributes from the requesting user
+	 * @return the json string of the game model
+	 */
 	public String joinGame(String json, UserAttributes ua);
 	
+	/**
+	 * Load game from a file indicated in the json
+	 *
+	 * @param json the json indicating which game to load from file
+	 * @param ua the user attributes from the requesting user
+	 * @return the string saying "Success"
+	 */
 	public String loadGame(String json, UserAttributes ua);
 	
+	/**
+	 * Saves the game to the specified filename
+	 *
+	 * @param json the json indicating what the filename should be
+	 * @param ua the user attributes from the requesting user
+	 * @return the string "Success"
+	 */
 	public String saveGame(String json, UserAttributes ua);
 	
 }
