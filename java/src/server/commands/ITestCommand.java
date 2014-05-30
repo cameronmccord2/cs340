9c585c9897720597c5de6d0083cb5a5a311af304
Cameron McCord <cameronmccord2@gmail.com>
2014-05-29 22:50:45 -0600

    update game facade javadocs

--- java/src/server/commands/ITestCommand.java
+++ java/src/server/commands/ITestCommand.java
@@ -1,6 +1,14 @@
 package server.commands;
 
+/**
+ * The Interface ITestCommand.
+ */
 public interface ITestCommand {
 	
+	/**
+	 * Runs the appropriate commands to see that the command was sucessful
+	 *
+	 * @return true, if successful
+	 */
 	public boolean wasCommandSuccessful();
 }
--- java/src/server/facades/IGameFacade.java
+++ java/src/server/facades/IGameFacade.java
@@ -2,14 +2,45 @@ package server.facades;
 
 import server.models.UserAttributes;
 
+/**
+ * The Interface IGameFacade.
+ */
 public interface IGameFacade extends IServerModelFacade {
 	
+	/**
+	 * Gets the list of commands that have been execuded for the specified game.
+	 *
+	 * @param json the json
+	 * @param ua the ua
+	 * @return the commands
+	 */
 	public String getCommands(String json, UserAttributes ua);
 	
+	/**
+	 * Run commands that are in the format that the getCommands endponit returns. Allows replaying of events.
+	 *
+	 * @param json the json
+	 * @param ua the ua
+	 * @return the string
+	 */
 	public String runCommands(String json, UserAttributes ua);
 	
+	/**
+	 * Resets the game model to the create game state.
+	 *
+	 * @param json the json
+	 * @param ua the ua
+	 * @return the string
+	 */
 	public String reset(String json, UserAttributes ua);
 	
+	/**
+	 * Gets the game model that needs to be sent to the client for gameplay.
+	 *
+	 * @param json the json
+	 * @param ua the ua
+	 * @return the game model
+	 */
 	public String getGameModel(String json, UserAttributes ua);
 	
 }
