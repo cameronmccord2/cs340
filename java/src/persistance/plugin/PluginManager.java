8e5bc92c9ac6457a6241742f8a8634fac8f94c8d
Scott Daly <scottdaly@khan.mtc.byu.edu>
2014-06-10 14:07:16 -0600

    added plugin manager

---
+++ java/src/persistance/plugin/PluginManager.java
@@ -0,0 +1,34 @@
+package persistance.plugin;
+
+/**
+ * This is called from the server.java and takes a paramter as to which persistance model to use.
+ * It then parses the config file and creates a new instance of the appropriate storage persistance class.
+ * @author scottdaly
+ *
+ */
+public class PluginManager {
+	
+	
+	/**
+	 * Parses the config file based on the persistance type
+	 * @param persistanceType that is specified from the command line.
+	 */
+	public void parseConfig(String persistanceType){
+		
+	}
+	
+	/**
+	 * Calls initialize on the persistance object in order to start connections to storages
+	 */
+	public void initPersistance(){
+		
+	}
+	
+	/**
+	 * Returns the newly created persistance object to be passed to the Server Model Facade
+	 */
+	public void getPersistanceObject(){
+		
+	}
+
+}
