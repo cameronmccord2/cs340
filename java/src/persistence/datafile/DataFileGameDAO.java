b885608dfbdb57ac5057f0e117d303e4fbf5c440
lasaldan <lasaldan@gmail.com>
2014-06-16 09:39:43 -0600

    Added dataFile DAO for Games and Users.

--- java/src/persistence/datafile/DataFileGameDAO.java
+++ java/src/persistence/datafile/DataFileGameDAO.java
@@ -2,13 +2,33 @@ package persistence.datafile;
 
 import client.models.interfaces.IGame;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * The Class dataFileGameDAO.
  */
 public class DataFileGameDAO {
-	
+
+	private String gamesDir;
+	private String gameFilenameFormat;
+
+	public DataFileGameDAO() {
+		gamesDir = "games";
+		gameFilenameFormat = gamesDir + File.separator + "gameId%dstate%s.catanGameData";
+		File gameDir = new File(gamesDir);
+
+		// if the directory does not exist, create it
+		if (!gameDir.exists()) {
+			boolean success = gameDir.mkdir();
+			if(!success)
+				System.out.println("Couldn't make games directory");
+		}
+	}
+
 	/**
 	 * Write a new game to a dataFile. Extracts the gameId from the game and saves the
 	 * game in the following format:
@@ -18,7 +38,26 @@ public class DataFileGameDAO {
 	 * @param game the game to insert
 	 */
 	public void insertNewGame(IGame game){
-		
+		int gameId = game.getGameInfo().getId();
+
+		String filename = String.format(gameFilenameFormat, gameId, "beginning");
+
+		File dataFile = new File(filename);
+
+		// No need to check if game exists, we'll just overwrite it anyway
+		try {
+			OutputStream os = new FileOutputStream(dataFile, false);
+			OutputStream buffer = new BufferedOutputStream(os);
+			ObjectOutput output = new ObjectOutputStream(buffer);
+			dataFile.createNewFile();
+
+			output.writeObject(game);
+
+			output.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	/**
@@ -28,7 +67,26 @@ public class DataFileGameDAO {
 	 * @param game the game to update
 	 */
 	public void updateGame(IGame game){
-		
+		int gameId = game.getGameInfo().getId();
+
+		String filename = String.format(gameFilenameFormat, gameId, "current");
+
+		File dataFile = new File(filename);
+
+		// No need to check if game exists, we'll just overwrite it anyway
+		try {
+			OutputStream os = new FileOutputStream(dataFile, false);
+			OutputStream buffer = new BufferedOutputStream(os);
+			ObjectOutput output = new ObjectOutputStream(buffer);
+			dataFile.createNewFile();
+
+			output.writeObject(game);
+
+			output.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	/**
@@ -37,7 +95,19 @@ public class DataFileGameDAO {
 	 * @param gameId the game id to delete
 	 */
 	public void deleteGameById(Integer gameId){
-		
+
+		String filename;
+		File gameFile;
+
+		// Delete beginning game if exists
+		filename = String.format(gameFilenameFormat, gameId, "beginning");
+		gameFile = new File(filename);
+		gameFile.delete();
+
+		// Delete current game if exists
+		filename = String.format(gameFilenameFormat, gameId, "current");
+		gameFile = new File(filename);
+		gameFile.delete();
 	}
 	
 	/**
@@ -46,6 +116,29 @@ public class DataFileGameDAO {
 	 * @return List of all games found.
 	 */
 	public List<IGame> getAllGames(){
-		return null;
+
+		List<IGame> gamesList = new ArrayList<>();
+		byte[] gameData;
+
+		for (File dataFile : new File(gamesDir).listFiles()) {
+
+			// TODO Check if a current game exists before returning the beginning game
+			if (dataFile.isFile()) {
+				try {
+					gameData = Files.readAllBytes(Paths.get(dataFile.getPath()));
+					ByteArrayInputStream bis = new ByteArrayInputStream(gameData);
+					ObjectInput in = new ObjectInputStream(bis);
+					IGame game = (IGame)in.readObject();
+
+					gamesList.add(game);
+
+				} catch (IOException | ClassNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return gamesList;
+
 	}
 }
--- java/src/persistence/datafile/DataFileUserDAO.java
+++ java/src/persistence/datafile/DataFileUserDAO.java
@@ -2,6 +2,10 @@ package persistence.datafile;
 
 import client.models.User;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,6 +13,21 @@ import java.util.List;
  */
 public class DataFileUserDAO {
 
+	private String usersDir;
+	private String userFilenameFormat;
+
+	public DataFileUserDAO() {
+		usersDir = "users";
+		userFilenameFormat = usersDir + File.separator + "userId%d.catanGameData";
+		File userDir = new File(usersDir);
+
+		// if the directory does not exist, create it
+		if (!userDir.exists()) {
+			boolean success = userDir.mkdir();
+			if(!success)
+				System.out.println("Couldn't make users directory");
+		}
+	}
 	/**
 	 * Insert User as a new datafile in the following format:
 	 * filename: users/userId.dat
@@ -19,7 +38,27 @@ public class DataFileUserDAO {
 	 * @param user the user to add/update
 	 */
 	public void upsertUser(User user){
-		
+
+		int userId = user.getPlayerID();
+
+		String filename = String.format(userFilenameFormat, userId);
+
+		File userDataFile = new File(filename);
+
+		// No need to check if user exists, we'll just overwrite it anyway
+		try {
+			OutputStream os = new FileOutputStream(userDataFile, false);
+			OutputStream buffer = new BufferedOutputStream(os);
+			ObjectOutput output = new ObjectOutputStream(buffer);
+			userDataFile.createNewFile();
+
+			output.writeObject(user);
+
+			output.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	/**
@@ -28,6 +67,26 @@ public class DataFileUserDAO {
 	 * @return the all users in the dataFiles
 	 */
 	public List<User> getAllUsers(){
-		return null;
+		List<User> usersList = new ArrayList<>();
+		byte[] userData;
+
+		for (File dataFile : new File(usersDir).listFiles()) {
+
+			if (dataFile.isFile()) {
+				try {
+					userData = Files.readAllBytes(Paths.get(dataFile.getPath()));
+					ByteArrayInputStream bis = new ByteArrayInputStream(userData);
+					ObjectInput in = new ObjectInputStream(bis);
+					User player = (User)in.readObject();
+
+					usersList.add(player);
+
+				} catch (IOException | ClassNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return usersList;
 	}
 }
